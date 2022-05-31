bada296a5356c739187a37bc06f02d60dcaf66b0
yuval <abc@example.com>
2022-05-31 20:48:17 +0300

    added search and insert

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,20 +1,74 @@
 var express = require("express");
 var router = express.Router();
+var id=0;
 const recipes_utils = require("./utils/recipes_utils");
 
 router.get("/", (req, res) => res.send("im here"));
 
 
 /**
- * This path returns a full details of a recipe by its id
+ * Returns a full details of a recipe by its id
  */
 router.get("/:recipeId", async (req, res, next) => {
   try {
-    const recipe = await recipes_utils.getRecipeDetails(req.params.recipeId);
+    const recipe = await recipes_utils.getRecipeFullDetails(req.params.recipeId);
     res.send(recipe);
   } catch (error) {
     next(error);
   }
 });
+
+/**
+ * Returns a recipes preview that matches the query
+ */
+router.get("/search" , async(req, res) => {
+  const query = req.query;
+  try{
+    let search_results= await recipes_utils.search(query);
+    res.send(search_results);
+  }catch(error){
+    res.sendStatus(404);
+  }
+
+});
+/**
+ * Returns a 3 random recipes preview
+ */
+router.get("/random", async (req, res , next) => {
+  try{
+    let random_3_recipes = await recipes_utils.getRandomThreeRecipes();
+    res.send(random_3_recipes);
+  } catch (error) {
+    next(error);
+  }
+
+});
+
+router.post("/addRecipe", async (req, res) =>{
+  try{
+    let recipe_details = {
+    userID: req.session.user_id,
+    recipeID: id,
+    title: req.body.title,
+    recipeImage: req.body.recipeImage,
+    readyInMinutes: req.body.readyInMinutes,
+    totalLikes: '0',
+    vegen: req.body.vegen,
+    vegeterian: req.body.vegeterian,
+    glutenFree: req.body.glutenFree
+  }
+  id=id+1;
+  await DButils.execQuery(
+    `INSERT INTO recipes VALUES ('${recipe_details.recipeID}', '${recipe_details.title}', '${recipe_details.recipeImage}',
+    '${recipe_details.readyInMinutes}', '${recipe_details.totalLikes}', '${recipe_details.vegen}', '${recipe_details.vegeterian}','${recipe_details.glutenFree}')`
+  );
+  await DButils.execQuery(
+    `INSERT INTO MyRecipes VALUES ('${recipe_details.userID}', '${recipe_details.recipeID}')`
+  );
+  res.status(201).send({ message: "recipe created", success: true });
+} catch (error) {
+  next(error);
+}
+})
 
 module.exports = router;
--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const { response } = require("express");
 const api_domain = "https://api.spoonacular.com/recipes";
 
 
@@ -17,8 +18,96 @@ async function getRecipeInformation(recipe_id) {
         }
     });
 }
+async function search(qurey){
+    const name = qurey.name;
+    const amount = qurey.amount;
+    const filter = qurey.filter;
+    if( filter === 1 ){
+        const cuisine = qurey.cuisine;
+        const diet = qurey.diet;
+        const intolerances = qurey.intolerances;
+        const response = await axios.get(`${api_domain}/complexSearch`, {
+            params: {
+                query:name,
+                number: amount ,
+                cuisine: cuisine,
+                diet: diet,
+                intolerances: intolerances,
+                apiKey: process.env.spooncular_apiKey
+            }
+        });
+    }
+    else{
+        const response = await axios.get(`${api_domain}/complexSearch`, {
+            params: {
+                query:name,
+                number: amount ,
+                apiKey: process.env.spooncular_apiKey
+            }
+        });
+    }
+    recipes_ids=[];
+    results= response.results;
+    results.map((recipe) => recipe_ids.push(recipe.id))
+    return getRecipesPreview(recipe_ids);
+
+}
+
+async function getRandomRecipes(){
+    const response = await axios.get(`${api_domain}/random`,{
+        params: {
+            number: 10 ,
+            apiKey: process.env.spooncular_apiKey
+        }
+    });
+    return response;
+}
 
 
+async function getRecipesPreview(recipes){
+
+    return recipes.map((recipe)=>{
+        let recipe_details = recipe;
+        if(recipe.data){
+            recipe_details = recipe.data;
+        }
+        const{
+            id,
+            title,
+            readyInMinutes,
+            image,
+            aggregateLikes,
+            vegan,
+            vegetarian,
+            glutenFree
+        } = data;
+        return {
+            id: id,
+            title: title,
+            image: image,
+            readyInMinutes: readyInMinutes,
+            popularity: aggregateLikes,
+            vegan: vegan,
+            vegetarian: vegetarian,
+            glutenFree: glutenFree
+
+        }
+    });
+
+}
+async function getRandomThreeRecipes(){
+    let random_recipes= await getRandomRecipes();
+    let filtered_recipes= random_recipes.filter((recipe) => (recipe.instructions != "") && (recipe.image && recipe.aggregateLikes && recipe.vegan && recipe.vegetarian && recipe.glutenFree))
+    if(filtered_recipes <3 ){
+        return getRandomThreeRecipes();
+    }
+    return getRecipesPreview([filtered_recipes[0],filtered_recipes[1], filtered_recipes[2]]);
+}
+
+async function getRecipeFullDetails(recipe_id) {
+    let recipe_info = await getRecipeInformation(recipe_id);
+    return recipe_info.data;
+}
 
 async function getRecipeDetails(recipe_id) {
     let recipe_info = await getRecipeInformation(recipe_id);
@@ -40,6 +129,9 @@ async function getRecipeDetails(recipe_id) {
 
 
 exports.getRecipeDetails = getRecipeDetails;
+exports.getRandomThreeRecipes = getRandomThreeRecipes;
+exports.search = search;
+exports.getRecipeFullDetails = getRecipeFullDetails;
 
 
 
